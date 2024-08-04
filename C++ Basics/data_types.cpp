e056b3a4e3e631aab74c36e91b5d36c9ae1a8220
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-08-04 23:56:06 +0530

    Basics Of C++

---
+++ C++ Basics/data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=10;
+    long b=11331;
+    long long c=1288388229;
+    float d=1.4;
+    string s="Hello";
+
+    cout<<"Integer:"<< a<<endl;
+    cout<<"Long:"<< b<<endl;
+    cout<<"Long Long:"<< c<<endl;
+    cout<<"Float:"<< d<<endl;
+    cout<<"String:"<< s;
+    return 0;
+}
