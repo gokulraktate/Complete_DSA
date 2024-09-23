c6f53db8aa17655c2f14c9ec4ea8162080416edb
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-09-23 22:57:49 +0530

    recursion

---
+++ Recursion/rec4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//  reverse the array using recursion
+int a[10];
+void f(int a[],int l, int r){
+    if(l>=r){
+        return ;
+    }
+    swap(a[l],a[r]);
+    f(a,l+1,r-1);
+}
+
+int main()
+{
+    int a[10],n;
+    cout<<"Enter the array size:"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    f(a,0,n-1);
+
+    for(int i=0;i<n;i++)
+    cout<<a[i];
+
+    return 0;
+}
