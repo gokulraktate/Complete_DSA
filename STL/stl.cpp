d8d55f8f55521dad9afbb1e1b6354aab7f4c5f12
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-10-03 21:12:46 +0530

    Update stl.cpp

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>   
 using namespace std;
 
+// STL divided  into 4 parts
+// 1 algorithm
+// 2 Containers
+// 3 Function
+// 4 Iterators
 
 
 //pairs
@@ -22,9 +27,9 @@ void pairs(){
 //vectors
 
 void vectors(){
-    vector<int> v;               //{}
+    vector<int> v;               //{}       Size of vector is not fixed as it is dynamic
     v.push_back(1);              //{1}
-    v.emplace_back(2);           //{1,2}
+    v.emplace_back(2);           //{1,2}  similar to pushback()  and it is faster
 
     // v.push_back({1,2});
     v.emplace_back(1,2);
@@ -42,12 +47,59 @@ void vectors(){
 
     it=it+2;
     cout<<*(it)<<" ";
+
+
     vector<int>::iterator it=v.end();
+    // vector<int>::iterator it=v.rend();
+    // vector<int>::iterator it=v.rbegin();
 
     cout<<v[0]<<" "<<v.at(0);
     cout<<v.back()<<" ";
     
     }
+
+
+    // auto
+
+    for(auto it=v.begin();it !=v.end();it++){
+        cout<<*(it)<<" ";
+    }
+
+    for(auto it:v){
+        cout<<it<<" ";
+    }
+
+    // erase
+
+    v.erase(v.begin()+1);
+    // {10,20,30,40,50,60}
+    v.erase(v.behin()+2,v.begin()+4)   //{10,20,50,60}
+
+
+    //insert function
+    vector<int>v(2,100);  //{100,100}
+    v.insert(v.begin(),300);  //{300,100,100}
+    v.insert(v.begin()+1,2,10);  //{300,10,10,100,100}
+
+
+    // copy
+    vector<int. copy(2,50);   //{50,50}
+    v.insert(v.begin(),copy.begin(),copy.end());   //{50,50,300,10,10,100,100}
+
+    // pop
+    // {10,20}
+    v.pop_back();   //{10}
+
+
+    //v1 ->{10,20}
+    //v2 ->{30,40}
+    v1.swap(v2);  //v1-> {30,40}  v2->{10,20}
+
+    v.clear();  //erase all
+
+    cout<<v.empty();   //if empty then TRUE otherwise FALSE
+
+    
 
 
 int main(){
