6199445ed889cc651fd742d7170918a9a24a0806
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-10-07 22:09:15 +0530

    Map in STL

---
+++ STL/map.cpp
@@ -0,0 +1,36 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+//  void explainmap(){
+//     map <int , int> mpp;    //key and value pair
+
+//     map <int ,pair<int,int>>mpp;
+
+//     map <pair<int,int>,int>mpp;
+
+
+//     mpp[1]=2;     //for key 1 value is 2
+
+//     mpp.emplace({3,1});    //for key 3 value 1
+//     mpp.insert({2,4});     //for key 2 value 4
+
+//     mpp[{2,3}]=10;     //for key(2,3) value is 10
+
+    
+
+//     for(auto it:map){
+//         cout<<it.first<<" "<<it.second<<endl;
+
+//     }
+
+//     cout<<mpp[1];
+//     cout<<mpp[5];
+
+
+
+//  }
+ 
+// int main()
+// {
+//     return 0;
+// }
