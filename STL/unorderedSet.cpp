f36508f0c648d29755079cbece021bbe75480e06
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-10-07 22:08:51 +0530

    set in STL

---
+++ STL/unorderedSet.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ void ExpUnorderedSet(){
+    unordered_set<int> st;
+
+    //same as simple set but data is stored in random order
+    //not sorted
+
+ }
+
+
+int main()
+{
+    return 0;
+}
