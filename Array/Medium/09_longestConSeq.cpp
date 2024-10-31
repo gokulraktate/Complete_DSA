5103343cca1c9ad53977e262862a0fe4429d1a67
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2024-10-31 13:37:30 +0530

    Update 09_longestConSeq.cpp

--- Array/Medium/09_longestConSeq.cpp
+++ Array/Medium/09_longestConSeq.cpp
@@ -42,11 +42,75 @@
 
 
 
+//better
+
+
 #include <bits/stdc++.h>
 using namespace std;
  
- 
-int main()
-{
-    return 0;
-}
+ class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        int cnt=0,longest=1,prev=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(nums[i]-1==prev){
+                cnt++;
+                prev=nums[i];
+            }
+            else if(nums[i]!=prev){
+                cnt=1;
+                prev=nums[i];
+            }
+            longest=max(longest,cnt);
+        }
+        return longest;
+    }
+};
+
+// int main()
+// {
+//     return 0;
+// }
+
+
+
+
+//optimal
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==0){
+//             return 0;
+//         }
+//         int cnt=0;
+//         int longest=1;
+
+//         unordered_set<int> st;
+//         for(int i=0;i<n;i++){
+//             st.insert(nums[i]);
+//         }
+//         for(auto it: st){
+//             if(st.find(it-1)==st.end()){
+//                 cnt=1;
+//                 int x=it;
+
+//                 while(st.find(x+1)!=st.end()){
+//                     x=x+1;
+//                     cnt++;
+
+//                 }
+                
+//             }
+//             longest=max(cnt,longest);
+//         }
+        
+//         return longest;
+//     }
+// };
