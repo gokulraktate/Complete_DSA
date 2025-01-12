d61133065d00b9444456c3d300425ac23c545ddd
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2025-01-12 21:32:47 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ String/09_MaxNestingDepthofPar.cpp
@@ -0,0 +1,18 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int cnt=0;
+//         int maxi=0;
+
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 cnt++;
+//             }
+//             if(s[i]==')'){
+//                 cnt--;
+//             }
+//             maxi=max(maxi,cnt);
+//         }
+//         return maxi;
+//     }
+// };
