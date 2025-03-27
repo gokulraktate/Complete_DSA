25551b765429b720bc4ff3432853ab132f251c13
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2025-03-27 23:44:57 +0530

    Count Nodes

---
+++ 12_Binary_Tree/Traversal/01_CountNodes.cpp
@@ -0,0 +1,9 @@
+// int countNodes(int i) {
+//         // your code here
+//         if(i==1){
+//             return 1;
+//         }
+//         else{
+//             return pow(2,i-1);
+//         }
+//     }
