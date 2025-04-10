80ad5789ebdf2ca9fcce49e8a64dbfb84fc5bd2f
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2025-04-10 15:33:07 +0530

    Basics of C++

---
+++ C++ Basics/basic_structure.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>  
+// for importing all libraries
+
+using namespace std;
+
+int main(){
+    // cout<<"This is Simple Output"<<endl;
+    int x;
+    cin>>x;
+    cout<<"the value of x is :"<<x;
+    
+    return 0;
+    
+}
