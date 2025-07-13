27669b0cfb92977bff032826c29967c48bb5f291
Gokul Raktate <143199029+gokulraktate@users.noreply.github.com>
2025-07-13 15:54:42 +0530

    Subarray Sum Equals K

---
+++ Array/Medium/13_SubArraySum.cpp
@@ -0,0 +1,19 @@
+/*
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt{{0, 1}};
+        int ans=0;
+        int sum=0;
+
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans+=cnt[sum-k];
+            cnt[sum]++;
+        }
+        return ans;
+    }
+};
+
+*/
